2bc5a60111d9a8b99bf1b08a687720a704839e6e
julien spileers <spileers.julien59@gmail.com>
2022-01-11 18:36:45 +0100

    Creation des differente page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class BlogController extends AbstractController
 {
-    #[Route('/', name: 'blog')]
+    #[Route('/', name: 'Home')]
     public function index(PostRepository $ripo): Response
     {
 
@@ -19,5 +22,92 @@ class BlogController extends AbstractController
 
             'post' => $posts,
         ]);
+    }
+    #[Route('/Aperitif', name: 'Aperitif')]
+    public function Aperitif(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+        // $posts = $ripo->findByAll(array(), array('id' => 'desc'), 5, 0);
+
+        return $this->render('blog/Aperitif.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/Digestif', name: 'Digestif')]
+    public function Digestif(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+        // $posts = $ripo->findByAll(array(), array('id' => 'desc'), 5, 0);
+
+        return $this->render('blog/Digestif.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/Stimulant', name: 'Stimulant')]
+    public function Stimulant(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+
+        return $this->render('blog/Stimulant.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/Rafraichissant', name: 'Rafraichissant')]
+    public function Rafraichissant(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+
+        return $this->render('blog/Rafraichissant.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/CocktailHiver', name: 'CocktailHiver')]
+    public function CocktailHiver(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+
+        return $this->render('blog/CocktailHiver.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/CocktailSansAlcool', name: 'CocktailSansAlcool')]
+    public function CocktailSansAlcool(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+
+        return $this->render('blog/CocktailSansAlcool.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/Contact', name: 'Contact')]
+    public function Contact(PostRepository $ripo): Response
+    {
+        $posts = $ripo->findAll();
+
+        return $this->render('blog/Contact.html.twig', [
+
+            'post' => $posts,
+        ]);
+    }
+    #[Route('/Show/{id}', name: 'Show')]
+    public function Show(PostRepository $ripo, $id): Response
+    {
+        // $ripo = $this->getDoctrine()->getRepository(Post::class);
+        $post = $ripo->find($id);
+
+
+        // printf($id);
+
+        return $this->render('blog/Show.html.twig', [
+            'post' => $post,
+            'id' => $id
+        ]);
+        // printf('$id');
     }
 }
