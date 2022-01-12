f0e9d39292605e603efef60aa02dbbb1dd5cac66
julien spileers <spileers.julien59@gmail.com>
2022-01-12 22:03:19 +0100

    Ajouts page Show Post + twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
 class BlogController extends AbstractController
 {
     #[Route('/', name: 'Home')]
@@ -21,6 +20,26 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
 
             'post' => $posts,
+            'title1' => "Nos derniers cocktails",
+            'title2' => "Qui sommes nous",
+            'title3' => "Les plus consultés",
+            'title4' => "Nos cocktails",
+            'aboutTitle' => "What is Lorem Ipsum?",
+            'aboutContent' => " Ipsum is Loremsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type spimen book
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            'btnPost' => "Lire la suite",
+            'newsTitle' => "Ne manquer plus aucune recette",
+            'newsBtn' => "Envoyer",
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
+            'partageTitle' => "Partager nous vos recettes",
         ]);
     }
     #[Route('/Aperitif', name: 'Aperitif')]
@@ -32,6 +51,16 @@ class BlogController extends AbstractController
         return $this->render('blog/Aperitif.html.twig', [
 
             'post' => $posts,
+            'title' => 'Nos Apéritifs',
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/Digestif', name: 'Digestif')]
@@ -41,8 +70,17 @@ class BlogController extends AbstractController
         // $posts = $ripo->findByAll(array(), array('id' => 'desc'), 5, 0);
 
         return $this->render('blog/Digestif.html.twig', [
-
             'post' => $posts,
+            'title' => 'Nos Digestifs',
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/Stimulant', name: 'Stimulant')]
@@ -53,6 +91,16 @@ class BlogController extends AbstractController
         return $this->render('blog/Stimulant.html.twig', [
 
             'post' => $posts,
+            'title' => 'Nos Cocktails Stimulants',
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/Rafraichissant', name: 'Rafraichissant')]
@@ -63,6 +111,16 @@ class BlogController extends AbstractController
         return $this->render('blog/Rafraichissant.html.twig', [
 
             'post' => $posts,
+            'title' => 'Nos Cocktails Rafraichissants',
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/CocktailHiver', name: 'CocktailHiver')]
@@ -73,6 +131,16 @@ class BlogController extends AbstractController
         return $this->render('blog/CocktailHiver.html.twig', [
 
             'post' => $posts,
+            'title' => "Nos Cocktails d'hiver",
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/CocktailSansAlcool', name: 'CocktailSansAlcool')]
@@ -82,7 +150,18 @@ class BlogController extends AbstractController
 
         return $this->render('blog/CocktailSansAlcool.html.twig', [
 
+
             'post' => $posts,
+            'title' => 'Nos Cocktails Sans Alcool',
+            'btn' => 'voir',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
     #[Route('/Contact', name: 'Contact')]
@@ -92,22 +171,32 @@ class BlogController extends AbstractController
 
         return $this->render('blog/Contact.html.twig', [
 
+
             'post' => $posts,
+            'title' => 'Contact',
+            'catNav1' => "Apéritif",
+            'catNav2' => "Digestif",
+            'catNav3' => "Stimulant",
+            'catNav4' => "Rafraichissant",
+            'catNav5' => "Cocktail d'hiver",
+            'catNav6' => "Cocktail sans alcool",
+            'contact' => "Contact",
+            'home' => "Accueil",
         ]);
     }
-    #[Route('/Show/{id}', name: 'Show')]
-    public function Show(PostRepository $ripo, $id): Response
+    #[Route('/Show-{id}', name: 'Show')]
+
+    public function Show(Post $post): Response
     {
-        // $ripo = $this->getDoctrine()->getRepository(Post::class);
-        $post = $ripo->find($id);
+        // $em = $this->getDoctrine()->getManager();
+        // $em->remove($post);
+        // $em->flush();
+        // $ripo->findAll($id);
 
 
-        // printf($id);
 
         return $this->render('blog/Show.html.twig', [
             'post' => $post,
-            'id' => $id
         ]);
-        // printf('$id');
     }
 }
